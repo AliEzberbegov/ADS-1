ad7406693dbdb5c80b906a714bcc97fbc9d903c7
Ali <76096715+AliEzberbegov@users.noreply.github.com>
2021-03-24 19:16:06 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,17 +1,16 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  int left = 0; 
+  int left = 0;
   int right = size - 1;
   int count = 0;
-  
   while (left <= right) {
     int central = left + (right - left) / 2;
     if (arr[central] > value) {
     left = central - 1;
     } else if (arr[central] < value) {
     right = central + 1;
-    } 
+    }
     if (arr[central] == value) {
     while (arr[central] == value) {
     count = count + 1;
@@ -25,5 +24,5 @@ int cbinsearch(int *arr, int size, int value) {
       return count;
     }
   }
-  return 0; // если ничего не найдено
+  return 0;// если ничего не найдено
 }
