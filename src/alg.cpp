ad375c72f2a571a5f0e46b9270f8a6aba688df11
Ali <76096715+AliEzberbegov@users.noreply.github.com>
2021-03-24 19:01:19 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -7,23 +7,24 @@ int cbinsearch(int *arr, int size, int value) {
   while (left <= right) {
     int central = left + (right - left) / 2;
     if (arr[central] > value) {
-        left = central - 1;
-      } else if (arr[central] < value) {
-        right = central + 1;
-      } else {
-        while (arr[central] == value) {
-        count = count + 1;
-        central = central + 1;
-        }
-        central = central - count;
-        while (arr[central - 1] == value) {
-        count = count + 1;
-        central = central - 1;
-        }
-     }
+    left = central - 1;
+    } else if (arr[central] < value) {
+    right = central + 1;
+    } 
+    if (arr[central] == value) {
+    while (arr[central] == value) {
+    count = count + 1;
+    central = central + 1;
+    }
+    central = central - count;
+    while (arr[central - 1] == value) {
+    count = count + 1;
+    central = central - 1;
+    }
   }
-  if (count > 0) 
-   return count;
-    else
-   return 0; // если ничего не найдено
-  }
+}
+if (count > 0) 
+  return count;
+   else
+  return 0; // если ничего не найдено
+}
